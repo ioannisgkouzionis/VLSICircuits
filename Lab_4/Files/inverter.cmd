16543d289258c5618141b478742d4c7b22a239ee
igkouzionis <39958816+igkouzionis@users.noreply.github.com>
2018-12-07 14:14:40 +0200

    changes

---
+++ Lab_4/Files/inverter.cmd
@@ -0,0 +1,17 @@
+h Vdd
+l GND
+analyzer in out Vdd GND
+s
+h in
+s
+l in
+s
+h in
+s
+l in
+s
+h in
+s
+l in
+stepsize 2
+s
---
+++ Lab_4/Files/poly_delay.al
@@ -0,0 +1,4 @@
+= input inverter_0/in
+= output inverter_1/out
+= Vdd! inverter_0/Vdd
+= Vdd! inverter_1/Vdd
